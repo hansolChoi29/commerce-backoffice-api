aaeab2535e79a0c13a39cd241df1cccafbc2d778
hansolChoi29 <ggksthf29@gmail.com>
2026-01-23 16:07:27 +0900

    feat: entity 필드 생성

--- src/main/java/com/example/ledger/domain/product/entity/Product.java
+++ src/main/java/com/example/ledger/domain/product/entity/Product.java
@@ -2,6 +2,9 @@ package com.example.ledger.domain.product.entity;
 
 import jakarta.persistence.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "product")
 public class Product {
@@ -11,4 +14,23 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long id;
+
+    @Column(name = "sku", nullable = false)
+    private String sku; // 상품코드(관리목적)
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name; // 상품명
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false, length = 20)
+    private ProductStatus status; // 판매상태
+
+    @Column(name = "sale_price", nullable = false)
+    private BigDecimal salePrice; // 판매 단가 (매출 시)
+
+    @Column(name = "cost_price", nullable = false)
+    private BigDecimal costPrice; // 매입 원가 (발주 시)
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/com/example/ledger/domain/product/entity/ProductStatus.java
@@ -0,0 +1,5 @@
+package com.example.ledger.domain.product.entity;
+
+public enum ProductStatus {
+    ACTIVE, INACTIVE;
+}
