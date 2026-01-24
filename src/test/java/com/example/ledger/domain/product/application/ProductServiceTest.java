60ecc80d0a6e69b66680504b4175389b25abb4c7
hansolChoi29 <ggksthf29@gmail.com>
2026-01-24 14:49:45 +0900

    test: create fail test success (이미 존재하는 예외, sku/save 모두 금지)

--- src/test/java/com/example/ledger/domain/product/application/ProductServiceTest.java
+++ src/test/java/com/example/ledger/domain/product/application/ProductServiceTest.java
@@ -16,6 +16,7 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,5 +54,33 @@ class ProductServiceTest {
         verify(productRepository).existsByName("name");
         verify(skuGenerator).generate();
         verify(productRepository).save(any(Product.class));
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 예외, sku/save 모두 금지")
+    void create_name_duplicate() throws Exception {
+        // 실패 시나리오 1
+        // 이미 존재하는 상품이면 예외 + sku 생성도 / 저장도 하면 안 됨
+
+        // 1) 실패 조건
+        // 2) 실패 조건을 만족시키려면 mock을 어떻게 만들어야 하는가?
+        // 3) 실패했으면 절대 실행되면 안 되는 다음 단계는 무엇인가?
+
+        CreateCommand command = new CreateCommand(
+                "name",
+                BigDecimal.valueOf(12000),
+                BigDecimal.valueOf(13000)
+        );
+        given(productRepository.existsByName("name")).willReturn(true);
+
+        // [핵심]이 예외는 터져야 한다
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> productService.create(command)
+        );
+
+        verify(productRepository).existsByName("name");
+        verify(skuGenerator, never()).generate(); // 호출되면 안 된다
+        verify(productRepository, never()).save(any(Product.class));
     }
 }
